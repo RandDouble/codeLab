19580e6841770d0027aac2d551834f20c7e8b488
RandDouble <77081052+RandDouble@users.noreply.github.com>
2021-03-08 20:46:27 +0100

    sviluppo del processo grafico in python, tentatico di CMakeList fallimentare, inserimento delle potenzialità di calcolo automatico

--- generazioneFileProva/plotter.py
+++ generazioneFileProva/plotter.py
@@ -1,28 +1,69 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pan
+import os
+import time 
 
-a=pan.read_csv('../exeCalcolo/molla1-40g.csv', decimal=',',delimiter='\t').to_numpy()
-b=pan.read_csv('../exeCalcolo/molla1-40g.dat', decimal=',',delimiter='\t').to_numpy()
-#a=np.loadtxt('../exeCalcolo/molla1-40g.csv', converters = {0: lambda s: float(s.decode("UTF-8").replace(",", "."))})
-#b=np.genfromtxt('/home/pilo/codeLab/exeCalcolo/molla1-40g.dat')
-print(a)
-#print(b)
-at=a[:,0]
-bt=b[:,0]
-av=a[:,2]
-bv=b[:,2]
-#ax=10*a[:,1]
-#aa=10*a[:,3]
+def stampa(punti, grafico, file):
+#funzione che si occupa di stampare il grafico e di salvarlo
+    #creazione dei vettori
+    at=punti[:,0]
+    bt=grafico[:,0]
+    av=punti[:,2]
+    bv=grafico[:,2]
+    #inizio grafica
+    fig=plt.figure()
+    plt.plot( at, av,'bo',label='punti stazionari')
+    plt.plot(bt,bv,'y', label='grafico dei dati')
+    plt.title('estremi della funzione misurata')
+    plt.legend()
+    plt.show()
+    #salvataggio dei dati
+    if os.name=='nt':
+        fig.savefig("outputGrafici\\"+file+".png")
+    else:
+        fig.savefig("outputGrafici/"+file+".png")
 
-print(a)
-plt.figure()
-#plt.subplot(111)
-#plt.plot(at,ax, 'bo')
-#plt.show()
-#plt.subplot(212)
-plt.plot( at, av,'bo', bt,bv,'y')
-plt.show()
-#plt.subplot(313)
-#plt.plot( at,aa, 'r--')
-#plt.show()
+def controlloCartelle():
+#controlla che esistano le cartelle di partenza e se non esistono le fa
+    directory=str(os.getcwd())
+    print("la directory di partenza è : "+directory)
+    if os.path.isdir("outputGrafici")==False:
+        os.mkdir("outputGrafici")
+        print("creata la cartelle di output dei grafici")
+    else:
+        print("bene cartella output grafico già presente")
+    if os.path.isdir("massimi")==False:
+        os.mkdir("massimi")
+        print("creata la cartelle di calcolazione dei massimi dei grafici")
+    else:
+        print("bene cartella dei massimi è già presente")
+    if os.path.isdir("caricaFile")==False:
+        os.mkdir("caricaFile")
+        print("creata la cartella di caricamento dei file")
+    else:
+        print("bene cartella di caricamento già presente")
+
+def letturaDati(file):
+#legge e carica i dati dalle cartelle di cui prima
+    if os.name=='nt':
+        district=os.getcwd()
+        a=pan.read_csv(district+'\\massimi\\'+file+'.csv', decimal=',',delimiter='\t').to_numpy()
+        b=pan.read_csv(district+'\\caricaFile\\'+file+'.dat', decimal=',',delimiter='\t').to_numpy()
+    else:
+        a=pan.read_csv(os.getcwd()+'/massimi/'+file+'.csv', decimal=',',delimiter='\t').to_numpy()
+        b=pan.read_csv(os.getcwd()+'/caricaFile/'+file+'.dat', decimal=',',delimiter='\t').to_numpy()
+    return a, b
+
+##########parte principale del programma##############
+controlloCartelle()
+input("caricare i file nella cartella indicata, poi premere enter")
+elFile=os.listdir(path='caricaFile')
+# os.execv()
+time.sleep(3)
+##########parte di raccolta dati e stampa#############
+for file in elFile:
+    a, b=letturaDati(file[:-4])
+    stampa(a,b,file)
+print("programma terminato, grazie dell'uso,\nRicordati di ringraziare e venerare il mio sommo Creatore")
+#########termine del programma########################
